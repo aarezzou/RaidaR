67390ca04d74a92cb57b5e15e29242193877b0d3
aarezou <arezou_fatemi@sfu.ca>
2021-03-26 17:00:19 -0700

    rainy

--- 0.Rainy/Part1/test folder/move.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-lst = os.listdir("./")
-for fl in lst:
-    if fl.endswith("g"):
-        flder_name = fl[:-4]
-        os.mkdir(flder_name)
-        shutil.copyfile(fl, flder_name + '/' + fl)
